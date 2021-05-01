c37aed775435bcf5ec6bdf51aa7f8fdaadf202f5
Fareeda Mhadmhan <48981676+fareedamhadmhan@users.noreply.github.com>
2021-05-01 23:03:38 +0700

    1

--- register_system/login_db.php
+++ register_system/login_db.php
@@ -11,7 +11,7 @@
         if (empty($username)) {
             array_push($errors, "Username is required");
         }
-        //Test
+        //TestfdfdsdgsgsGSd
         if (empty($password)) {
             array_push($errors, "Password is required");
         }
