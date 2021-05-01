fdd5ab7056b41b79848fdd03e0d0681f7ee1a3bb
Kittiphop Sompuech <kittiphopsompuech2@gmail.com>
2021-05-02 00:22:50 +0700

    Merge branch 'develop' of https://github.com/fareedamhadmhan/project2 into develop

--- register_system/register_db.php
+++ register_system/register_db.php
@@ -30,6 +30,8 @@
         //Test
         //fada
         //earth
+        //fada
+        //earth
 
         $user_check_query = "SELECT * FROM users WHERE username = '$username' OR email = '$email' LIMIT 1";
         $query = mysqli_query($conn, $user_check_query);
